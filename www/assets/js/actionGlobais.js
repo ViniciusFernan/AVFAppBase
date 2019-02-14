3b42a963bfa560cf5a723356ae85467998b5d430
Vinicius <Viniciusblink@hotmail.com>
2019-02-14 12:11:17 -0200

    correção globais

--- www/assets/js/actionGlobais.js
+++ www/assets/js/actionGlobais.js
@@ -1,5 +1,5 @@
 $('body').on('tap', '.removeCard', function () {
-    var $this = $('#cardAvisos');
+    var $this = $(this).closest('#cardAvisos');
     $($this).removeClass('bounceInDown').addClass('bounceOutUp').delay(1000).queue(function() {
         $($this).remove();
     });
